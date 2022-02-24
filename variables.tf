e865c595cbebfd458ea36742857f626fa61bd53e
paetoe <52591618+paetoe@users.noreply.github.com>
2022-02-24 11:34:32 +0630

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 //AWS Configuration
-variable access_key {AKIA4CXCSAHDOTBBMB5H}
-variable secret_key {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}
+variable "access_key" {accesss_key = "AKIA4CXCSAHDOTBBMB5H"}
+variable "secret_key" {secret_key = "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"}
 
 variable "region" {
   default = "eu-west-1"
